0e138aa846ab3a4580e6fcd141f3586659c4225e
brice <briceguard-git@yahoo.com>
2019-08-10 16:18:18 +0100

    bound sleep properly with min / max

--- api/garmin/runbackfill.php
+++ api/garmin/runbackfill.php
@@ -8,7 +8,7 @@ if( isset( $argv[1] ) && isset( $argv[2] )){
     $token_id = $process->validate_input_id($argv[1]);
     $days = MIN(intval($argv[2]),90); // no more than 90 days
     if( isset( $argv[3] ) ){
-        $sleep = MAX(MIN(intval($argv[3]),60),300);
+        $sleep = MAX(MIN(intval($argv[3]),300),3);
     }else{
         $sleep = 100;
     }
