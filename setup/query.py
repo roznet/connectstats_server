b103fc2313d272e1df7d0df284cc940742592221
brice <briceguard-git@yahoo.com>
2019-08-11 10:21:22 +0100

    more option for query.py

--- setup/query.py
+++ setup/query.py
@@ -16,6 +16,9 @@ import argparse
 class ConnectStatsRequest:
 
     def __init__(self):
+
+        self.verbose = False
+        
         regexp = re.compile("'([a-zA-Z_]+)' +=> +'([-0-9a-zA-Z_!]+)'," )
         config = dict()
         with open( '../api/config.php' ) as cf:
@@ -55,12 +58,10 @@ class ConnectStatsRequest:
         url_base = parsed.scheme + "://" + parsed.netloc + parsed.path
         
         nonce = self.id_generator(16)
-
         now = str(int(time.time()))
 
         oauthmethod = "HMAC-SHA1"
         oauthver = "1.0"
-
 
         oauth_params ={"oauth_consumer_key":self.consumerKey,
                        "oauth_token" :self.userAccessToken,
@@ -71,34 +72,42 @@ class ConnectStatsRequest:
 
         all_params = dict(oauth_params)
         all_params.update( get_params )
-
         all_params_order = sorted(all_params.keys())
 
         params = '&'.join( [ '{}={}'.format( x, urllib.parse.quote(all_params[x]) )  for x in all_params_order ] )
-        
         base = '&'.join( [ method, urllib.parse.quote(url_base, safe=''), urllib.parse.quote( params ) ] )
-
         key = '&'.join( [ urllib.parse.quote(self.consumerSecret), urllib.parse.quote(self.userAccessTokenSecret) ] )
 
         digest = hmac.new( key.encode('utf-8'), base.encode('utf-8'), hashlib.sha1 ).digest()
         signature = base64.b64encode( digest )
 
         oauth_params['oauth_signature'] = signature
-
         oauth_params_order = sorted( oauth_params.keys() )
-
         header_params = ', '.join( [ '{}="{}"'.format( x, urllib.parse.quote( oauth_params[x] ) ) for x in oauth_params_order] )
-        
         header = 'OAuth {}'.format( header_params )
-
         headers = {'Authorization': header}
         
-        request = urllib.request.Request(accessUrl, headers=headers )
-        
-        response = urllib.request.urlopen( request )
-        
-        return( response.read() )
+        return( headers )
 
+    def query_url(self, accessUrl ):
+        headers = self.authentification_header( accessUrl )
+        
+        request = urllib.request.Request(accessUrl, headers=headers )
+
+        try:
+            if self.verbose:
+                print( '> Request {}'.format( accessUrl ) )
+            response = urllib.request.urlopen( request )
+            contents = response.read()
+            if self.verbose:
+                print( '> Received {} bytes'.format( len(contents) ) )
+        except urllib.error.HTTPError as e:
+            if self.verbose:
+                print( '> Error: {}'.format( str( e ) ) )
+            contents = str( e )
+            
+        return( contents )
+        
 
 
 
@@ -107,11 +116,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser( description='Query ConnectStats API', formatter_class=argparse.RawTextHelpFormatter )
     parser.add_argument( 'url' )
     parser.add_argument( '-t', '--token', help='Token id for the access Token Secret', default = 1 )
+    parser.add_argument( '-o', '--outfile', help='file to save output' )
+    parser.add_argument( '-v', '--verbose', help='verbose output', action='store_true' )
     args = parser.parse_args()
     
-    a = ConnectStatsRequest()
+    req = ConnectStatsRequest()
+    req.verbose = args.verbose 
 
-    a.setup_token_id( args.token )
-
-    print( a.authentification_header( args.url ) )
+    req.setup_token_id( args.token )
+    content = req.query_url( args.url ) 
+    if args.outfile:
+        with open( args.outfile, 'wb' ) as of:
+            of.write( content )
+            if req.verbose:
+                print( '> Saved {}'.format( args.outfile ) )
+    else:
+        print( content.decode('utf-8' ) )
+        
+             
 
