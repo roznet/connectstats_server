93f57df3be42269d7d30544610b67b109f721b75
brice <briceguard-git@yahoo.com>
2019-11-21 06:58:40 -0500

    only run fit extract if within 24h

--- api/garmin/runfitextract.php
+++ api/garmin/runfitextract.php
@@ -16,16 +16,21 @@ if( isset( $argv[1] ) ){
     
     foreach( $cbids as $arg_file_id ){
         $file_id = $process->validate_input_id($arg_file_id);
-    
-        $data = $process->query_file( NULL, NULL, $file_id );
+        //only extract if within 24h
+        if( $process->should_fit_extract( $file_id, 24.0 ) ){
+            $data = $process->query_file( NULL, NULL, $file_id );
 
-        if( strlen( $data ) ){
-            try {
-                $fit = new adriangibbons\phpFITFileAnalysis( $data, array( 'input_is_data' => true ) );
-                $process->fit_extract( $file_id, $fit->data_mesgs );
-            }catch(Exception $e ){
-                printf( "ERROR: can't process fit file for id %d. Error: %s", $file_id, $e->getMessage() );
+            if( strlen( $data ) ){
+                try {
+                    $fit = new adriangibbons\phpFITFileAnalysis( $data, array( 'input_is_data' => true ) );
+                    $process->fit_extract( $file_id, $fit->data_mesgs );
+                }catch(Exception $e ){
+                    printf( "ERROR: can't process fit file for id %d. Error: %s", $file_id, $e->getMessage() );
+                }
             }
+        }else{
+            printf( 'INFO: skipping extract for %d as appears too old'.PHP_EOL, $file_id );
+
         }
     }
 }
