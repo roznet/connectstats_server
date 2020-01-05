fd8179014c75253ea4e9448db9caed762fb1f4ca
brice <briceguard-git@yahoo.com>
2020-01-05 14:22:42 +0000

    maintenance to cleanup logs

--- setup/maintenance.sh
+++ setup/maintenance.sh
@@ -1,10 +1,24 @@
-#!/bin/bash
+#!/bin/zsh
 
 # this script is intended to be run regularly to clean up files and do maintenance
 
-if [ -d ../api/garmin/tmp ]; then
-	echo "Deny from all" > ../api/garmin/tmp/.htaccess
-	/bin/find ../api/garmin/tmp -mtime +15 -delete
-fi
+function cleanup {
+		target=${1}
+		
+		if [ -d ${target} ]; then
+				echo "Cleaning tmp directory ${target}"
+				echo "  files: `/usr/bin/find ${target} | wc -l`"
+				echo "  size:  `/usr/bin/du -h ${target}`"
+				echo "Deny from all" > ../api/garmin/tmp/.htaccess
+				/usr/bin/find ${target} -mtime +15 -delete
+				echo "After cleanup ${target}"
+				echo "  files: `/usr/bin/find ${target} | wc -l`"
+				echo "  size:  `/usr/bin/du -h ${target}`"
+				echo
+		fi
+}
+cleanup ../api/garmin/tmp
+cleanup ../api/garmin/log
+
 
 
