a1af4e2d11b777598af9fe6cbf5614bc5f839ebb
brice <briceguard-git@yahoo.com>
2020-03-21 09:51:58 +0000

    easier debugkey setup

--- setup/debugkey.php
+++ setup/debugkey.php
@@ -27,9 +27,24 @@ if( $process->ensure_commandline($argv,1) ){
     }
     $where = implode( ' OR ', $filter );
     $query = sprintf( 'SELECT token_id,cs_user_id,userAccessToken, userAccessTokenSecret FROM tokens WHERE %s', $where );
-    print( $query.PHP_EOL );
     $rv = $process->sql->query_as_array( $query );
-    print( json_encode( $rv ) . PHP_EOL);
+    if( $rv ){
+        $file = 'debugkey.json';
+        if( is_file( $file ) ){
+            $added = $rv[0]['token_id'];
+            $previous = json_decode( file_get_contents( $file ), true );
+
+            foreach( $previous as $one){
+                if( $one['token_id'] != $added){
+                    array_push( $rv, $one );
+                }
+            }
+        }
+        printf( 'Wrote %d keys into %s'.PHP_EOL, count( $rv ), $file );
+        file_put_contents( $file, json_encode( $rv ) );
+    }else{
+        printf( 'Query result empty %s'.PHP_EOL, $query );
+    }
 }else{
 
     header('HTTP/1.1 403 Forbidden');
